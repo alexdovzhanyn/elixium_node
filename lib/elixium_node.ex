d9dfc7a2e0b222c39539e69384adc67cec85a84a
Alex Dovzhanyn <dovzhanyn.alex@gmail.com>
2018-08-30 00:17:56 -0400

    validate blocks as they come in

--- lib/elixium_node.ex
+++ lib/elixium_node.ex
@@ -1,5 +1,9 @@
 defmodule ElixiumNode do
   use GenServer
+  require Logger
+  alias Elixium.Validator
+  alias Elixium.Blockchain
+  alias Elixium.Blockchain.Block
 
   def start_link(chain) do
     GenServer.start_link(__MODULE__, chain)
@@ -14,13 +18,37 @@ defmodule ElixiumNode do
   end
 
   def handle_info(msg, {supervisor, chain}) do
-    case msg do
-      header = %{type: "BLOCK_HEADER"} ->
-        IO.puts "matched header"
-      block = %{type: "BLOCK"} ->
-        IO.inspect block
-      _ -> IO.puts "Didnt match"
-    end
+    chain =
+      case msg do
+        header = %{type: "BLOCK_HEADER"} ->
+          IO.inspect header
+          chain
+        block = %{type: "BLOCK"} ->
+          IO.inspect Block.header(block)
+
+          difficulty = List.first(chain).difficulty
+
+          difficulty =
+            if rem(block.index, Blockchain.diff_rebalance_offset()) == 0 do
+              new_difficulty = Blockchain.recalculate_difficulty(chain) + difficulty
+              IO.puts("Difficulty recalculated! Changed from #{difficulty} to #{new_difficulty}")
+              new_difficulty
+            else
+              difficulty
+            end
+
+          case Validator.is_block_valid?(block, chain, difficulty) do
+            :ok ->
+              Logger.info("Block #{block.index} valid.")
+              Blockchain.add_block(chain, block)
+            err ->
+              Logger.info("Block #{block.index} invalid!")
+              chain
+          end
+        _ ->
+          IO.puts "Didnt match"
+          chain
+      end
 
     {:noreply, {supervisor, chain}}
   end

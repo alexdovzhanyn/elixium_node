583dedfb26077851d9a38750af46068178a22fd7
Alex Dovzhanyn <dovzhanyn.alex@gmail.com>
2018-08-31 00:46:45 -0400

    forgot a comma

--- mix.exs
+++ mix.exs
@@ -23,7 +23,7 @@ defmodule ElixiumNode.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:elixium_core, "~> 0.2"}
+      {:elixium_core, "~> 0.2"},
       # {:local_dependency, path: "../core", app: false},
       {:logger_file_backend, "~> 0.0.10"}
     ]

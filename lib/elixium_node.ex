7de7f2a5b0f535336e718735751ac2e661ea67a4
Alex Dovzhanyn <dovzhanyn.alex@gmail.com>
2018-09-12 22:59:00 -0400

    update to align with ets usage in core

--- lib/elixium_node.ex
+++ lib/elixium_node.ex
@@ -4,48 +4,42 @@ defmodule ElixiumNode do
   alias Elixium.Validator
   alias Elixium.Blockchain
   alias Elixium.Blockchain.Block
+  alias Elixium.Store.Ledger
 
   def start_link(chain) do
-    GenServer.start_link(__MODULE__, chain)
+    GenServer.start_link(__MODULE__, {})
   end
 
-  def init(chain) when is_list(chain) do
-    {:ok, chain}
-  end
+  def init(_state), do: {:ok, {}}
 
-  def handle_info(msg, chain) do
-    chain =
-      case msg do
-        header = %{type: "BLOCK_HEADER"} ->
-          IO.inspect header
-          chain
-        block = %{type: "BLOCK"} ->
-          IO.inspect Block.header(block)
+  def handle_info(msg, _state) do
+    case msg do
+      header = %{type: "BLOCK_HEADER"} ->
+        IO.inspect header
+      block = %{type: "BLOCK"} ->
+        IO.inspect Block.header(block)
 
-          difficulty = List.first(chain).difficulty
+        last_block = Ledger.last_block()
 
-          difficulty =
-            if rem(block.index, Blockchain.diff_rebalance_offset()) == 0 do
-              new_difficulty = Blockchain.recalculate_difficulty(chain) + difficulty
-              IO.puts("Difficulty recalculated! Changed from #{difficulty} to #{new_difficulty}")
-              new_difficulty
-            else
-              difficulty
-            end
-
-          case Validator.is_block_valid?(block, chain, difficulty) do
-            :ok ->
-              Logger.info("Block #{block.index} valid.")
-              Blockchain.add_block(chain, block)
-            err ->
-              Logger.info("Block #{block.index} invalid!")
-              chain
+        difficulty =
+          if rem(block.index, Blockchain.diff_rebalance_offset()) == 0 do
+            new_difficulty = Blockchain.recalculate_difficulty() + last_block.difficulty
+            IO.puts("Difficulty recalculated! Changed from #{last_block.difficulty} to #{new_difficulty}")
+            new_difficulty
+          else
+            last_block.difficulty
           end
-        _ ->
-          IO.puts "Didnt match"
-          chain
-      end
 
-    {:noreply, chain}
+        case Validator.is_block_valid?(block, difficulty) do
+          :ok ->
+            Logger.info("Block #{block.index} valid.")
+            Blockchain.add_block(block)
+          err -> Logger.info("Block #{block.index} invalid!")
+        end
+      _ ->
+        IO.puts "Didnt match"
+    end
+
+    {:noreply, {}}
   end
 end

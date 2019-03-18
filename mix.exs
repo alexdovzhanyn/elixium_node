e756768ba38d1f1f1b5fa9e1aed31fdd81e485de
Alex Dovzhanyn <dovzhanyn.alex@gmail.com>
2019-03-18 14:29:40 -0400

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ElixiumNode.MixProject do
   def project do
     [
       app: :elixium_node,
-      version: "1.1.2",
+      version: "1.1.3",
       elixir: "~> 1.7",
       start_permanent: true,
       deps: deps()
